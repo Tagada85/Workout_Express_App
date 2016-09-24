ebaa6de8e604981ec3d94d14f6506e5275f4dd2a
DamCosset <dcosset@hotmail.fr>
2016-09-24 11:56:52 +0200

    Add console.log

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ passport.use(new TwitterStrategy({
     callbackURL: "https://127.0.0.1:3000/auth/twitter/return"
   },
   function(token, tokenSecret, profile, done) {
+    console.log(profile);
     User.findOneAndUpdate({ username: profile.displayName},
       {
         username: profile.displayName
